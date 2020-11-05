c4ea78b7c1faf6b2de00afc5609eafbc86c370a7
moisesISAL <moises@isaldigital.com>
2020-11-05 12:03:44 -0600

    instalando jwt

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -18,11 +19,34 @@ user.post('/', async (req, res, next) => {
     return res.status(500).json({ code: 500, message: 'Campos incompletos' });
 });
 
-user.get("/", async(req,res,next)=>{
+user.post('/login', async (req, res, next) => {
+    const { user_mail, user_password } = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}'`;
+    const rows = await db.query(query);
+    console.log(rows);
+    if (user_mail && user_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({ code: 200, message: token });
+        } else {
+            return res.status(401).json({ code: 401, message: 'Usuario y/o contrasena incorrectos' });
+        }
+    }
+    return res.status(500).json({ code: 500, message: 'Campos incompletos' });
+
+
+
+
+});
+
+user.get("/", async (req, res, next) => {
     const query = "SELECT * FROM user";
     const rows = await db.query(query);
 
-    return res.status(200).json({code:200, message:rows});
+    return res.status(200).json({ code: 200, message: rows });
 });
 
 module.exports = user; //Entre comillas anade al arbol de dependencias nuestro archivo a las librerias
